8af0c468f299753627e391b0aa2e25c69af90de8
Muhammad Imtiaz Hussain <emmihssn@gmail.com>
2019-11-06 19:28:14 +0500

    Python Programing

---
+++ Assignment_5.py
@@ -0,0 +1,17 @@
+print("Assignment_5")
+print('---------------------------------')
+First_name = "Imtiaz"
+Second_name = "Hussain"
+Full_name = (First_name +" "+ Second_name)
+print (Full_name)
+
+print("---------------Straight----------")
+#============================================
+First_name = "Imtiaz"
+Second_name = "Hussain"
+Full_name = (Second_name +" "+ First_name)
+print (Full_name)
+print("---------------Reverse----------")
+
+print('---------------------------------')
+print("Assignment By: Imtiaz Hussain")
---
+++ Assignment_6.py
@@ -0,0 +1,8 @@
+print("Assignment_6")
+print("=========================")
+user_input1 = int(input("Enter First Value   "))
+user_input2 = int(input("Enter Second Value  "))
+Result = 'Your Result is',(user_input1 + user_input2)
+print(Result)
+
+
